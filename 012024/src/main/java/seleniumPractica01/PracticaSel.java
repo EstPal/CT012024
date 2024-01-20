31e0035ffad7eaa41722a32edb5786d14efb92d6
AIONTECH <AIONTECH@DESKTOP-VT9L6NG>
2024-01-20 10:56:07 -0600

    Primer commit

---
+++ 012024/src/main/java/seleniumPractica01/PracticaSel.java
@@ -0,0 +1,15 @@
+package seleniumPractica01;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class PracticaSel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
